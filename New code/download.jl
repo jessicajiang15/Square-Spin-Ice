7158acdd9c26063087a768d49a656b72fd8a7afd
Jessica Jiang <jessica@Jessicas-MacBook-Pro-6.local>
2026-01-13 14:26:14 -0800

    All packages required to use the repository.

---
+++ New code/download.jl
@@ -0,0 +1,38 @@
+using Pkg
+Pkg.add("Arpack")
+Pkg.add("LaTeXStrings")
+Pkg.add("LinearAlgebra")
+Pkg.add("Plots")
+Pkg.add("SparseArrays")
+Pkg.add("LinearAlgebra")
+Pkg.add("Test")
+Pkg.add("TensorCore")
+Pkg.add("TensorOperations")
+Pkg.add("TensorOperations")
+Pkg.add("ITensors")
+Pkg.add("NearestNeighbors")
+Pkg.add("StaticArrays")
+Pkg.add("Distances")
+Pkg.add("LinearAlgebra")
+Pkg.add("KrylovKit")
+
+
+Pkg.add([
+    "Arpack",
+    "Plots",
+    "JLD",
+    "KrylovKit",
+    "Distributions",
+    "Convex",
+    "SCS",
+    "Combinatorics",
+    "BenchmarkTools",
+    "LsqFit",
+    "Images",
+    "TestImages",
+    "FileIO",
+    "JLD2",
+    "PlotThemes",
+    "TimerOutputs",
+])
+
